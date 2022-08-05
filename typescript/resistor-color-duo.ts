55b8e388cdd9485a7ed358562961df6a2c874709
Nelson Estevão <nelsonmestevao@proton.me>
2022-08-06 00:03:23 +0100

    [typescript]: Add first exercise

---
+++ typescript/resistor-color-duo.ts
@@ -0,0 +1,30 @@
+type Color =
+  | "black"
+  | "brown"
+  | "red"
+  | "orange"
+  | "yellow"
+  | "green"
+  | "blue"
+  | "violet"
+  | "grey"
+  | "white";
+
+const mapper = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+};
+
+export function decodedValue(colors: Color[]): number {
+  return parseInt(
+    colors.slice(0, 2).reduce((acc, color) => acc + mapper[color], "")
+  );
+}
