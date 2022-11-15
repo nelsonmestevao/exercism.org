2c09543b11f884a29b22ab66b1e07f26ce96f1c6
Nelson Estevão <nelsonmestevao@proton.me>
2022-11-15 11:19:23 +0000

    Upgrade Elixir and Erlang

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -5,7 +5,7 @@ defmodule Exercism.MixProject do
     [
       app: :exercism,
       version: "1.0.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases(),
