e60c14db4a6fe58fe0f620194f225b550fd07aaa
Nelson Estevão <nelsonmestevao@proton.me>
2022-11-22 19:39:18 +0000

    [go]: Add cars exercise

---
+++ golang/cars/cars_assemble.go
@@ -0,0 +1,21 @@
+package cars
+
+// CalculateWorkingCarsPerHour calculates how many working cars are
+// produced by the assembly line every hour.
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+	return (float64(productionRate) * successRate) / float64(100.0)
+}
+
+// CalculateWorkingCarsPerMinute calculates how many working cars are
+// produced by the assembly line every minute.
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+	return int(CalculateWorkingCarsPerHour(productionRate, successRate) / float64(60))
+}
+
+// CalculateCost works out the cost of producing the given number of cars.
+func CalculateCost(carsCount int) uint {
+	batching := carsCount / 10
+	remaining := carsCount % 10
+
+	return uint(batching * 95000 + remaining * 10000)
+}
