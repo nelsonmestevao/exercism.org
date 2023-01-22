51db1f84dc7d3b0d5be2414691314123dc9ee635
Nelson Estevão <nelsonmestevao@proton.me>
2023-01-22 00:28:54 +0000

    [go]: improve solution code

--- go/vehicle-purchase/vehicle_purchase.go
+++ go/vehicle-purchase/vehicle_purchase.go
@@ -7,20 +7,25 @@ func NeedsLicense(kind string) bool {
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
 func ChooseVehicle(option1, option2 string) string {
-	if option1 <= option2 {
-		return option1 + " is clearly the better choice."
-	}
+	return min(option1, option2) + " is clearly the better choice."
+}
 
-	return option2 + " is clearly the better choice."
+func min(first string, second string) string {
+	if first <= second {
+		return first
+	}
+	return second
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
 func CalculateResellPrice(originalPrice, age float64) float64 {
 	if age < 3 {
-		return 0.8 * originalPrice
-	} else if age < 10 {
-		return 0.7 * originalPrice
-	} else {
-		return 0.5 * originalPrice
+		return originalPrice * 0.8
 	}
+
+	if age < 10 {
+		return originalPrice * 0.7
+	}
+
+	return originalPrice * 0.5
 }
